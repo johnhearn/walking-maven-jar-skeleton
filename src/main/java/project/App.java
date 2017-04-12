ebabb1c14198c52153029ac59605fe12701b07f6
John Hearn <hearn.john@gmail.com>
2017-04-12 19:30:49 +0200

    Make acceptance test pass by adding class with main()

---
+++ src/main/java/project/App.java
@@ -0,0 +1,14 @@
+package project;
+
+import java.io.IOException;
+
+/**
+ * Main entry point to the application.
+ */
+public class App {
+
+	public static void main(String[] args) throws IOException {
+		// Do nothing
+	}
+	
+}
